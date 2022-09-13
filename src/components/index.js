85c49836b594b415d4f13de02ca183e2b83eed2b
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-13 08:48:37 -0700

    commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+export { Button } from "./Button"
+export {Cart} from "./Cart";
+export { ChartsHeader } from "./ChartsHeader";
+export { Chat } from "./Chat";
+export { Footer } from "./Footer";
+export { Header } from "./Header";
+export { Navbar } from "./Navbar";
+export { Notification } from "./Notification";
+export { Sidebar } from "./Sidebar";
+export { ThemeSettings } from "./ThemeSettings";
+export { UserProfile } from "./UserProfile";
+export { LineChart } from "./LineChart";
+export { PieChart } from "./PieChart";
+export { SparkLine } from "./charts/SparkLine";
+export { StackedChart } from "./charts/StackedChart";
