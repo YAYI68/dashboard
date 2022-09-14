af5cefce97091103c54cdd77e87415700afa1bd9
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-14 11:23:55 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
           <div className="fixed md:static bg-main-bg dark:bg-main-bg navbar w-full">
             < Navbar />
           </div>
-         </div>
+       
 
          <div>
            <Routes>
@@ -74,7 +74,7 @@ function App() {
             
            </Routes>
          </div>
-
+        </div>   
       </div>
      </BrowserRouter>
     </div>
