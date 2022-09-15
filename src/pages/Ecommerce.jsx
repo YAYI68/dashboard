62fd8eb110dfcf9d0f97c6a7ad180c10d5944abc
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-15 04:22:20 -0700

    commit

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go'; 
 import { Stacked,PieChart, Button,SparkLine} from "../components"
 import { earningData, SparkLineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../context/ContextProvider';
@@ -9,9 +11,7 @@ export const  Ecommerce = () => {
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
         <div className='bg-white dark:text-gray-200 
         dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 
-        pt-9 bg-hero-pattern bg-no-repeat bg-cover bg-center
-                                   
-        '>
+        pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
           
         </div>
         
