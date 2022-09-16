9d0679180c7d9e987dea50134cbd2dbb34aa3f60
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-16 01:12:31 -0700

    commit

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { ScheduleComponent, ViewsDirective,
+   ViewDirective, Day, Week, WorkWeek, Month, Agenda, 
+   Inject, Resize, DragAndDrop
+} from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+import { Header } from '../components';
+import { scheduleData } from '../data/dummy';
+
+
+
 
 export const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Calendar" />
+      <ScheduleComponent>
+         <Inject services={[Day, Week, Month,WorkWeek,
+          Agenda,Resize,DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
+   
   )
 }
